0871fd63e4d1a417c9907336f9346c85b1824759
Steve Trettel <stevejtrettel@gmail.com>
2025-12-29 17:53:27 -0800

    updates to book, with a shortcode

---
+++ _extensions/shader-demo/shader-demo.lua
@@ -0,0 +1,33 @@
+return {
+  ['shader-demo'] = function(args, kwargs, meta)
+    local name = pandoc.utils.stringify(args[1])
+    local height = pandoc.utils.stringify(kwargs["height"] or "500px")
+    local caption = pandoc.utils.stringify(kwargs["caption"] or "")
+    
+    local base_path = "/demos/" .. name
+    
+    if quarto.doc.is_format("html") then
+      local id = "demo-" .. name:gsub("[^%w]", "-")
+      
+      return pandoc.RawBlock("html", string.format([[
+<figure class="shader-demo">
+  <div id="%s" style="width: 100%%; height: %s;"></div>
+  <script type="module">
+    import { embed } from '%s/embed.js';
+    await embed({ container: '#%s' });
+  </script>
+  %s
+</figure>
+]], id, height, base_path, id, 
+        caption ~= "" and string.format("<figcaption>%s</figcaption>", caption) or ""))
+    else
+      return pandoc.Para({
+        pandoc.Image(
+          {pandoc.Str(caption)},
+          base_path .. "/screenshot.png",
+          caption
+        )
+      })
+    end
+  end
+}
