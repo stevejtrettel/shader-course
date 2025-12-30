b954535631d0ce7f7053d5e10116cbc07ceb0b52
Steve Trettel <stevejtrettel@gmail.com>
2025-12-30 07:47:57 -0800

    working on new extension

--- _extensions/shader-code/shader-code.lua
+++
@@ -1,8 +0,0 @@
--- Minimal shader-code extension
-
-return {
-  ['shader-code'] = function(args, kwargs, meta)
-    local name = pandoc.utils.stringify(args[1])
-    return pandoc.CodeBlock("-- Placeholder for: " .. name, {class = "glsl"})
-  end
-}
